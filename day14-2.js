271c92611ad8f5aac2ae33cea1a8565f7c9bf916
Niels Simenon <niels.simenon@gmail.com>
2021-12-15 05:47:25 +0100

    Removed redundant intermediate pairs from day 14-2.

--- day14-2.js
+++ day14-2.js
@@ -10,66 +10,47 @@ async function init() {
     const sourcePairs = sourceTemplate.reduce((acc, element, index) => (sourceTemplate[index + 1] ? { ...acc, [`${element}${sourceTemplate[index + 1]}`]: 1 } : acc), {});
     const rules = Object.fromEntries(manual.slice(1).map((rule) => rule.split(/\s*->\s*/)));
 
-    const pairs = {};
+    const pairs = sourcePairs;
 
-    function step(prevPairs, maxSteps, steps) {
-	const stepPairs = {};
-
-	Object.entries(prevPairs).forEach(([pair, count], index) => {
+    function step(maxSteps, steps) {
+	Object.entries(pairs).forEach(([pair, count], index) => {
 	    if (pairs[pair] > 0) {
 		pairs[pair] -= count;
 	    }
 
-	    const pairLeft = pair.length > 1 && `${pair.slice(0, 1)}${rules[pair]}`;
-	    const pairRight = pair.length > 1 && `${rules[pair]}${pair.slice(1)}`;
+	    const pairLeft = pair.length > 1 && `${pair.charAt(0)}${rules[pair]}`;
+	    const pairRight = pair.length > 1 && `${rules[pair]}${pair.charAt(1)}`;
 
 	    if (pairLeft) {
 		pairs[pairLeft] = (pairs[pairLeft] || 0) + count;
-		stepPairs[pairLeft] = (stepPairs[pairLeft] || 0) + count;
 	    }
 
 	    if (pairRight) {
 		pairs[pairRight] = (pairs[pairRight] || 0) + count;
-		stepPairs[pairRight] = (stepPairs[pairRight] || 0) + count;
 	    }
 	})
 
 	if (steps < maxSteps) {
-	    step(stepPairs, maxSteps, steps + 1);
+	    step(maxSteps, steps + 1);
 	}
     }
 
     console.time('duration');
 
-    step(sourcePairs, 40, 1);
+    step(40, 1);
 
     const countEntries = Object.entries(pairs);
 
-    const total = countEntries.reduce((acc, [pair, count]) => acc + count, 0);
-    const elements = countEntries.reduce((acc, [pair, count], index, array) => ({
-	...acc,
-	[pair.charAt(1)]: (acc[pair.charAt(1)] || 0) + count,
-    }), {});
+    const length = countEntries.reduce((acc, [pair, count]) => acc + count, 0);
+    const elements = countEntries.reduce((acc, [pair, count], index, array) => ({ ...acc, [pair.charAt(1)]: (acc[pair.charAt(1)] || 0) + count }), {});
 
-    const sortedElements = Object.entries(elements).reduce((acc, [element, count]) => {
-	if (!acc.most || count > acc.most[1]) {
-	    acc.most = [element, count];
-	}
-
-	if (!acc.least || count < acc.least[1]) {
-	    acc.least = [element, count];
-	}
-
-	return acc;
-    }, {
-	most: null,
-	least: null,
-    });
+    const mostCommon = Object.entries(elements).reduce((acc, [element, count]) => (!acc || count > acc[1] ? [element, count] : acc), null);
+    const leastCommon = Object.entries(elements).reduce((acc, [element, count]) => (!acc || count < acc[1] ? [element, count] : acc), null);
 
     console.log(pairs);
     console.log(elements);
-    console.log('length', total);
-    console.log('sum', sortedElements.most[1] - sortedElements.least[1]);
+    console.log('length', length);
+    console.log('sum', mostCommon[1] - leastCommon[1]);
     console.timeEnd('duration');
 }
 
